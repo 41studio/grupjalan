b33f7698c64be659698e3a0047075b03bbfa7c8c
Kris Dhinal <kris@41studio.com>
2015-09-18 16:39:04 +0700

    migrating database

--- db/migrate/20150918035306_remove_column_from_user.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnFromUser < ActiveRecord::Migration
-  def change
-    remove_column :users, :username, :string
-    remove_column :users, :first_name, :string
-    remove_column :users, :last_name, :string
-    remove_column :users, :neighborhood, :string
-  end
-end
