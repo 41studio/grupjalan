b13090395bc519083eee3a04bb2ee0e8b67d42c4
Dimas Julisa Taniawan <dimas@41studio.com>
2015-10-23 10:32:17 +0700

    disable unused generators

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module Grupjalan
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # disable generators
+    config.generators do |generate|
+      generate.helper false
+      generate.assets false
+      generate.view_specs false
+    end
   end
 end
