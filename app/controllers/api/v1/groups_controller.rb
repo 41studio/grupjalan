11ed2cdb189b8244ba7e0e57f992794d2cab6bed
Dimas Julisa Taniawan <dimas@41studio.com>
2015-11-18 10:52:12 +0700

    add pagination for search groups

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -7,6 +7,7 @@ class Api::V1::GroupsController < BaseApiController
     param :start_to_trip, String, "Start to trip for group, format: 'dd/mm/yyyy'"
     param :end_to_trip, String, "End to trip for group, format: 'dd/mm/yyyy'"
     param :name, String, "Group name"
+    param :page, String, "Pagination page number"
   end
 
   def_param_group :create_group do
@@ -26,13 +27,14 @@ class Api::V1::GroupsController < BaseApiController
   param_group :search_group
   def search
     @groups = Group.joins(:trips).where(
-      "LOWER(groups.name) ILIKE :name AND trips.start_to_trip = :start_to_trip AND trips.end_to_trip >= :end_to_trip", 
+      "LOWER(groups.name) ILIKE :name AND (trips.start_to_trip <= :start_to_trip AND trips.end_to_trip >= :end_to_trip)
+      OR (:start_to_trip <= trips.start_to_trip AND :end_to_trip >= trips.end_to_trip)", 
       {
         start_to_trip: params[:start_to_trip].to_date,
         end_to_trip: params[:end_to_trip].to_date,
         name: "%#{params[:name].downcase}%"
       }
-    )
+    ).page(params[:page])
   end
 
   api :GET, "/v1/groups/:id", 'get detail group'
