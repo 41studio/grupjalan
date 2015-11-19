6834f2bfdca9aa285a1d533a33a551d4e6164f48
Dimas Julisa Taniawan <dimas@41studio.com>
2015-11-19 10:40:27 +0700

    remove conflict comment, and regenerate annotate

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -24,47 +24,6 @@
 #  index_groups_on_user_id         (user_id)
 #
 
-<<<<<<< HEAD
-#  id         :integer          not null, primary key
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :integer
-#  location   :string
-#  lat        :float
-#  lng        :float
-#  photo      :string
-#  image      :string
-#  slug       :string
-=======
-#  id             :integer          not null, primary key
-#  name           :string
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  user_id        :integer
-#  location       :string
-#  lat            :float
-#  lng            :float
-#  photo          :string
-#  image          :string
-#  destination_id :integer
-#  slug           :string
->>>>>>> 4d29a427b4f50836ca7ab627803659d2d6c5f9f5
-#
-# Indexes
-#
-#  index_groups_on_slug  (slug) UNIQUE
-<<<<<<< HEAD
-=======
-#
-# Indexes
-#
-#  index_groups_on_category_id  (category_id)
-#  index_groups_on_trip_id      (trip_id)
-#  index_groups_on_user_id      (user_id)
->>>>>>> 4d29a427b4f50836ca7ab627803659d2d6c5f9f5
-#
-
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -35,56 +35,6 @@
 #  index_posts_on_user_id                  (user_id)
 #
 
-<<<<<<< HEAD
-#  id                      :integer          not null, primary key
-#  title                   :string
-#  photo                   :string
-#  video                   :string
-#  created_at              :datetime         not null
-#  updated_at              :datetime         not null
-#  user_id                 :integer
-#  description             :text
-#  trip_id                 :integer
-#  group_id                :integer
-#  comments_count          :integer          default(0)
-#  cached_votes_total      :integer          default(0)
-#  cached_votes_score      :integer          default(0)
-#  cached_votes_up         :integer          default(0)
-#  cached_votes_down       :integer          default(0)
-#  cached_weighted_score   :integer          default(0)
-#  cached_weighted_total   :integer          default(0)
-#  cached_weighted_average :float            default(0.0)
-=======
-#  id             :integer          not null, primary key
-#  title          :string
-#  photo          :string
-#  video          :string
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
-#  user_id        :integer
-#  description    :text
-#  trip_id        :integer
-#  group_id       :integer
-#  comments_count :integer          default(0)
->>>>>>> 4d29a427b4f50836ca7ab627803659d2d6c5f9f5
-#
-# Indexes
-#
-#  index_posts_on_cached_votes_down        (cached_votes_down)
-#  index_posts_on_cached_votes_score       (cached_votes_score)
-#  index_posts_on_cached_votes_total       (cached_votes_total)
-#  index_posts_on_cached_votes_up          (cached_votes_up)
-#  index_posts_on_cached_weighted_average  (cached_weighted_average)
-#  index_posts_on_cached_weighted_score    (cached_weighted_score)
-#  index_posts_on_cached_weighted_total    (cached_weighted_total)
-<<<<<<< HEAD
-=======
-#  index_posts_on_group_id                 (group_id)
-#  index_posts_on_trip_id                  (trip_id)
-#  index_posts_on_user_id                  (user_id)
->>>>>>> 4d29a427b4f50836ca7ab627803659d2d6c5f9f5
-#
-
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
