9bb7a22e782d0da7e65c306eaf433c21c250ea4b
Dimas Julisa Taniawan <dimas@41studio.com>
2015-11-20 10:45:18 +0700

    remove redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # before_action :redirect_https
-
-  # def redirect_https        
-  #   redirect_to :protocol => "https://" unless request.ssl?
-  #   return true              
-  # end
 
   rescue_from CanCan::AccessDenied do |exception|
     if request.format.json?
