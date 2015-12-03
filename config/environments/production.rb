d895e079d8f27d22f63c55504535be283cf5270c
kris dhinal <kris@41studio.com>
2015-12-03 11:53:45 +0700

    modified file on production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,11 +101,11 @@ Rails.application.configure do
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_KEY_ID'],
+      aws_access_key_id: 'AKIAJA24JJUKFLQDIC5Q',
+      aws_secret_access_key: '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',
     }
     
-    config.fog_directory = ENV['AWS_S3_BUCKET']
+    config.fog_directory = 'grupjalan'
   end
 
 end
