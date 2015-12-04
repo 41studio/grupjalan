65c476ed8c5a24bf9a9f6ec12098b543fff1c3f8
Dimas Julisa Taniawan <dimas@41studio.com>
2015-12-04 11:23:18 +0700

    fix join

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -14,6 +14,7 @@ class Ability
       can :create, [Group, Post, Comment]
       can :modify, [Post, Trip, Group, Comment], user_id: user.id
       can :join, [Group, Trip]
+      can :create, Trip
       can :votes, Post
       can :read, :all
     else
