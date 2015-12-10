8f5879f3c81029b15b4cbd5ca462e3e5dd6041a1
kris dhinal <kris@41studio.com>
2015-12-10 13:58:36 +0700

    sorting trips old and on the way trips in show page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,24 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!, only: :mytrips
+
   def index
     redirect_to mytrips_url if user_signed_in?
   end
 
   def mytrips
-    @trips = current_user.trips.includes(:group)
+    @trips = current_user.trips.includes(:group).where(
+      "end_to_trip >= :end_to_trip",
+      {
+        end_to_trip: Time.now
+      }
+    )
+
+    @old_trips = current_user.trips.includes(:group).where(
+      "end_to_trip < :end_to_trip",
+      {
+        end_to_trip: Time.now
+      }
+    )
 
   end
 end
